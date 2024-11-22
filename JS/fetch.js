80f71a403a991956086ad95ceb1e075636f48f55
Walmart Wan <roseslucerob@nclack.org>
2024-11-21 19:15:06 -0800

    .

---
+++ JS/fetch.js
@@ -0,0 +1,28 @@
+// A custom tool for fetching data from external sources
+
+
+
+async function FFFF(folder) {  //Fetch Files From Folder, gets list of filenames in a folder(used for dynamic assets)
+    const response = await UF('https://api.github.com/repos/ssh-lvl/new-shatter/contents/'+folder);
+    if (typeof(response != Array)) {
+        console.error('url fetch failed! :' + typeof(response) == null ? 'null' : response)
+        return null;
+    }
+    const Files = response
+        .filter(item => item.type === "file")
+        .map(file => file.name);
+    
+    return Files;
+}
+
+async function UF(api) { //URL Fetch(used as a base for fetches), simply returns the response from an api call
+    try {
+        const response = await fetch(api);
+        if (!response.ok) {
+            return null;
+        }
+        return response.json();
+    } catch (e) {
+        return e;
+    }
+}
