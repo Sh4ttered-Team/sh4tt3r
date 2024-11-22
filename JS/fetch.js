b80837fee0fd79a08c302a0cf24e69892eadc2bf
Walmart Wan <roseslucerob@nclack.org>
2024-11-22 13:54:56 -0800

    DB fetch

--- JS/fetch.js
+++ JS/fetch.js
@@ -15,9 +15,9 @@ async function FFFF(folder) {  //Fetch Files From Folder, gets list of filenames
     return Files;
 }
 
-async function UF(api) { //URL Fetch(used as a base for fetches), simply returns the response from an api call
+async function UF(api, headers = {}) {//(api required:url, headers optional:other headers like api keys) //URL Fetch(used as a base for fetches), simply returns the response from an api call
     try {
-        const response = await fetch(api);
+        const response = await fetch(api,headers);
         if (!response.ok) {
             return null;
         }
@@ -27,6 +27,20 @@ async function UF(api) { //URL Fetch(used as a base for fetches), simply returns
     }
 }
 
-async function FDB(table) { //Fetch DataBase, gets the 
-
+async function FDB(table) { //Fetch data from a table in the supabase
+    const SUPABASE_URL = 'https://fffwukshwgrcdyqmvahg.supabase.co'
+    const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZnd1a3Nod2dyY2R5cW12YWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzOTkxMjAsImV4cCI6MjA0Njk3NTEyMH0.MsMeFMkrJCeJzRFWMZXM-CZu8gwaScV7feentsgMQvI';
+    const response = await UF(`${SUPABASE_URL}/rest/v1/${table}`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'apikey': key,
+              'Authorization': `Bearer ${key}`
+        }})
+    
+    if (!Array.isArray(response)) {
+        console.error('url fetch failed! :' + typeof(response) == null ? 'null' : response)
+        return null;
+    }
+    return response;
 }
