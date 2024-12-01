e6a81ec8414617b5d03383b76c9e326934861f55
Walmart Wan <sshlvl.contact@gmail.com>
2024-12-01 00:31:40 -0800

    better main linking

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,19 @@
 const user = await FDB('profiles');
+let prefix
+if (window.location.href.startsWith('https://ssh-lvl.github.io/')) {
+    prefix = '/new-shatter';
+}
+else {
+    prefix = '';
+}
 if (user[0] == null) {
-    window.location.href = '/SUB/-Login.html' 
+    window.location.href = prefix + '/SUB/-Login.html' 
 }
 if (user[0].banned) {
-    window.location.href = '/404.html';
+    window.location.href = prefix + '/404.html';
 }
 
 if (!await loggedIn()) {
-    window.location.href = '/SUB/-Login.html';
+    window.location.href = prefix + '/SUB/-Login.html';
 }
 
