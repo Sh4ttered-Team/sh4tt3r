8de94212abef46c86ac694b355ccea28b35796c6
Walmart Wan <sshlvl.contact@gmail.com>
2024-11-30 18:13:48 -0800

    why?!?

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,11 @@
 const user = await FDB('profiles');
 if (user[0] == null) {
-    window.location.href = '/SUB/-Login.html'
+    window.location.href = '/new-shatter/SUB/-Login.html'
 }
 if (user[0].banned) {
-    window.location.href = '/404.html';
+    window.location.href = '/new-shatter/404.html';
 }
 
 if (!await loggedIn()) {
-    window.location.href = '/SUB/-Login.html';
+    window.location.href = '/new-shatter/SUB/-Login.html';
 }
