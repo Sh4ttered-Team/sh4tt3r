c440e47bdb66d21a6ab44fe06b18f1f015191727
Walmart Wan <sshlvl.contact@gmail.com>
2024-11-30 18:17:58 -0800

    .

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 const user = await FDB('profiles');
 if (user[0] == null) {
-    window.location.href = '/new-shatter/SUB/-Login.html'
+    window.location.href = '/new-shatter/SUB/-Login.html' 
 }
 if (user[0].banned) {
     window.location.href = '/new-shatter/404.html';
