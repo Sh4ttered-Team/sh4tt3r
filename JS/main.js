2ff89778a3d05fdae3d89e01cf60c7cb689598a8
ssh-lvl <sshlvl.contact@gmail.com>
2024-12-05 18:19:37 +0000

    .

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,23 @@
 const user = await FDB('profiles');
-let prefix
+let prefix = '';
+
 if (window.location.href.startsWith('https://ssh-lvl.github.io/')) {
     prefix = '/new-shatter';
 }
-else {
-    prefix = '';
-}
-if (user[0] == null) {
-    window.location.href = prefix + '/SUB/-Login.html'; 
-}
-if (user[0].banned) {
-    window.location.href = prefix + '/404.html';
-}
 
-if (!await loggedIn()) {
+if (user && user.size > 0) {
+    if (user[0].banned) {
+        window.location.href = prefix + '/404.html';
+    } else {
+        window.location.href = prefix + '/SUB/-Login.html';
+    }
+    if (user[0].admin) {
+        let erudaScript = document.createElement('script').src = '//cdn.jsdelivr.net/npm/eruda';
+        let erudaInit = document.createElement('script').type = 'text/javascript';
+        erudaInit.text = 'eruda.init();';
+        document.body.appendChild(erudaScript);
+        document.body.appendChild(erudaInit);
+    }
+} else if (!await loggedIn()) {
     window.location.href = prefix + '/SUB/-Login.html';
 }
-
