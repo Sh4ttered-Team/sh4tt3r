ca5438ec8b854dd366ef8d63f7130a871c43d809
ssh-lvl <sshlvl.contact@gmail.com>
2024-12-05 20:38:07 +0000

    .

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,33 @@
-const user = await FDB('profiles');
+const IsUserBypass = (document.getElementsByTagName('acc-bypass').length != 0) ? true : false;
+let user = await FDB('profiles');
 let prefix = '';
 
 if (window.location.href.startsWith('https://ssh-lvl.github.io/')) {
     prefix = '/new-shatter';
 }
 
-if (user && user.size > 0) {
+if (user) {
     if (user[0].banned) {
         window.location.href = prefix + '/404.html';
-    } else {
+    } else if (!user) {
         window.location.href = prefix + '/SUB/-Login.html';
     }
     if (user[0].admin) {
-        let erudaScript = document.createElement('script').src = '//cdn.jsdelivr.net/npm/eruda';
-        let erudaInit = document.createElement('script').type = 'text/javascript';
-        erudaInit.text = 'eruda.init();';
+        let erudaScript = document.createElement('script');
+        erudaScript.src = '//cdn.jsdelivr.net/npm/eruda';
+        erudaScript.async = true;
+    
         document.body.appendChild(erudaScript);
-        document.body.appendChild(erudaInit);
+    
+        erudaScript.onload = function() {
+            eruda.init();
+        };
+    
+        erudaScript.onerror = function() {
+            alert('eruda fail')
+        };
     }
+    
 } else if (!await loggedIn()) {
     window.location.href = prefix + '/SUB/-Login.html';
 }
