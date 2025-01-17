7fa2deaa66ca16d2ff2a16fb8eeec5d0e0684659
ssh-lvl <sshlvl.contact@gmail.com>
2025-01-17 10:13:35 -0800

    Update games.js

--- JS/games.js
+++ JS/games.js
@@ -38,8 +38,6 @@ export function moveGameDiv(gameDiv, isFav) {
     }
 
 export function create_game(game_name,game_url, suffix, favs) {
-    console.trace()
-    console.log(game_name, game_url, suffix, favs);
     const gameDiv = document.createElement('div');
     gameDiv.className = 'game';
     const favButton = document.createElement('button');
@@ -67,9 +65,6 @@ export function create_game(game_name,game_url, suffix, favs) {
 }
 
 export async function create_game_div(flash, url, game, favs) {
-    console.group("create_game")
-    console.trace()
-    console.log(flash, url, game, favs);
     let suffix = '';
     if (flash) {
     suffix = '&flash=true';
@@ -78,5 +73,4 @@ export async function create_game_div(flash, url, game, favs) {
     }
     const gameDiv = create_game(game.name, game.url, suffix, favs);
     document.querySelector(favs.includes(game.name) ? '.favGames' : '.gamesDiv').appendChild(gameDiv);
-    console.groupEnd()
 }
