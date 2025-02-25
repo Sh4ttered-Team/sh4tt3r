52e862e9dda592bcc2951984de1e1bf33a8e7515
ssh-lvl <sshlvl.contact@gmail.com>
2025-02-25 12:43:44 -0800

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -111,12 +111,18 @@ function addLoadingScreen() {
 			document.querySelector(".loading-overlay").style.visibility = "visible";
 			document.addEventListener("keydown", e => e.preventDefault());
 			document.body.style.pointerEvents = "none";
+			if document.querySelector(".send-button") {
+				document.querySelector(".send-button").disabled = true
+			}
 		};
 
 		window.hideLoadingScreen = function () {
 			document.querySelector(".loading-overlay").style.visibility = "hidden";
 			document.body.style.pointerEvents = "auto";
 			document.removeEventListener("keydown", e => e.preventDefault());
+			if document.querySelector(".send-button") {
+				document.querySelector(".send-button").disabled = false
+			}
 		};
 	}
 }
