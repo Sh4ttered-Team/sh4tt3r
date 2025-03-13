d1e6288e2963c9380983c00d94945dfd541e2041
ssh-lvl <sshlvl.contact@gmail.com>
2025-03-13 15:44:34 +0000

    .

--- JS/main.js
+++ JS/main.js
@@ -60,8 +60,8 @@ if (!ThemeBypass) {
 
 //fix for different web environments (local dev/github pages)
 window.prefix = "";
-if (window.location.href.startsWith("https://shattered-team.github.io/")) {
-	window.prefix = "/shatter";
+if (window.location.href.startsWith("https://shatt3red-team.github.io/")) {
+	window.prefix = "/shatt3r";
 }
 
 //if there isn't an account bypass for the page
